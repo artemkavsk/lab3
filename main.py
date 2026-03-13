1291a51b6173e1a5d73348619d756e322285e9fb
Artem Kuznetsov <artemka_vsk@gmail.com>
2026-03-13 09:17:28 +0300

    lab3

--- main.py
+++ main.py
@@ -20,20 +20,20 @@ def generate_key():
 # wndw
 root = tk.Tk()
 root.title("keygen")
-root.geometry("500x300")
+root.geometry("333x151")
 
 # png bg
 bg_photo = tk.PhotoImage(file="window.png")
-canvas = tk.Canvas(root, width=500, height=300)
+canvas = tk.Canvas(root, width=333, height=151)
 canvas.pack(fill="both", expand=True)
 canvas.create_image(0, 0, image=bg_photo, anchor="nw")
 
 # generate button
 button = tk.Button(root, text="generate key", command=generate_key)
-canvas.create_window(250, 100, window=button)
+canvas.create_window(333/2, 151/2, window=button)
 
 # output
 key_label = tk.Label(root, text="", font=("Arial", 16), bg="white")
-canvas.create_window(250, 180, window=key_label)
+canvas.create_window(333/2, 151/2+50, window=key_label)
 
 root.mainloop()
